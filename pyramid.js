48ff8e6f2a4bab4e9dbac101b9a7c1d63161b6ba
Mandela Mesele <meselemandela@gmail.com>
2021-07-18 23:32:43 +0300

    pyramid with stars

---
+++ pyramid.js
@@ -0,0 +1,14 @@
+function stars(n){
+    var str = '';
+    for(var i=1; i<=n; i++){
+        for(var k=1; k<=n-i; k++){
+            str += " ";
+        }
+        for(var j=1; j<=i; j++){
+            str += "*  ";
+        }
+        console.log(str);
+        str = "";
+    }
+}
+stars(4);
