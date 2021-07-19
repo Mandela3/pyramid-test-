63b9bcb7ce11ccdff862b5f5167f94c430c789d7
Mandela Mesele <meselemandela@gmail.com>
2021-07-19 09:17:31 +0300

    dimention changes

--- pyramid.js
+++ pyramid.js
@@ -2,13 +2,13 @@ function stars(n){
     var str = '';
     for(var i=1; i<=n; i++){
         for(var k=1; k<=n-i; k++){
-            str += " ";
+            str += "  ";
         }
         for(var j=1; j<=i; j++){
-            str += "*  ";
+            str += " *  ";
         }
         console.log(str);
-        str = "";
+        str = " ";
     }
 }
 stars(4);
